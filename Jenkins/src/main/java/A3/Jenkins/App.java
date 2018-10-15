ba91bd04f22bc8c6d8856d02423b8040189be432
admin <admin@156.59.50.181>
2018-10-16 12:11:12 +1300

    Part1

---
+++ Jenkins/src/main/java/A3/Jenkins/App.java
@@ -0,0 +1,13 @@
+package A3.Jenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
